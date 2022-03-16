1ee3fabf531f744356abfebc1ac5890c9800dc66
kuga01 <uga.kalu@gmail.com>
2022-03-16 08:42:48 +0000

    Added Terraform files to infra repo

---
+++ s3.tf
@@ -0,0 +1,20 @@
+resource "aws_s3_bucket" "my-s3-bucket" {
+
+  bucket_prefix = var.bucket_prefix
+
+  acl = var.acl
+
+  
+
+   versioning {
+
+    enabled = var.versioning
+
+  }
+
+  
+
+  tags = var.tags
+
+}
+
